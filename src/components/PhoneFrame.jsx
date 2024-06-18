5503d6f67d20b689cdb67ee9f37e758001f123a9
fisayo-adegun <fisayo.adegun@gmt-limited.com>
2024-06-18 15:47:30 +0100

    changed some images

--- src/components/PhoneFrame.jsx
+++ src/components/PhoneFrame.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import clsx from 'clsx'
 
-import frame from '@/images/WalletScreen1.png'
+import frame from '@/images/Dashboard.png'
 
 function PlaceholderFrame(props) {
   return (
