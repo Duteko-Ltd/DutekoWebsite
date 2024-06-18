5173a032c7c00b92c4d6be81011ae87600c153bf
galexp <40064291+galexp@users.noreply.github.com>
2024-06-18 03:22:36 +0100

    second

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,20 +24,18 @@ export function Footer() {
   return (
     <footer className="border-t border-gray-200">
       <Container>
-        {/* <div className="flex flex-col items-start justify-between gap-y-12 pb-6 pt-16 lg:flex-row lg:items-center lg:py-16">
+         <div className="flex flex-col items-start justify-between gap-y-12 pb-6 pt-16 lg:flex-row lg:items-center lg:py-16">
           <div>
-            <div className="flex items-center text-gray-900">
-              {/* <Logomark className="h-10 w-10 flex-none fill-cyan-500" /> 
+            {/* <div className="flex items-center text-gray-900">
+             <Logomark className="h-10 w-10 flex-none fill-cyan-500" /> 
               <div className="ml-4">
                 <p className="text-base font-semibold">Duteko</p>
                 <p className="mt-1 text-sm">Ready to experience a smarter way to deliver? Sign up as a sender or courier today and join the Duteko community!</p>
               </div>
-            </div>
-            <nav className="mt-11 flex gap-8">
-              {/* <NavLinks /> 
-            </nav>
+            </div> */}
+          
           </div>
-          <div className="group relative -mx-4 flex items-center self-stretch p-4 transition-colors hover:bg-gray-100 sm:self-auto sm:rounded-2xl lg:mx-0 lg:self-auto lg:p-6">
+          {/* <div className="group relative -mx-4 flex items-center self-stretch p-4 transition-colors hover:bg-gray-100 sm:self-auto sm:rounded-2xl lg:mx-0 lg:self-auto lg:p-6">
             <div className="relative flex h-24 w-24 flex-none items-center justify-center">
               <QrCodeBorder className="absolute inset-0 h-full w-full stroke-gray-300 transition-colors group-hover:stroke-cyan-500" />
               <Image src={qrCode} alt="" unoptimized />
@@ -53,8 +51,8 @@ export function Footer() {
               Ready to experience a smarter way to deliver? Sign up as a sender or courier today and join the Duteko community!
               </p>
             </div>
-          </div>
-        </div> */}
+          </div> */}
+        </div> 
         <div className="flex flex-col items-center border-t border-gray-200 pb-12 pt-8 md:flex-row-reverse md:justify-between md:pt-6">
           {/* <form className="flex w-full justify-center md:w-auto">
             <TextField
@@ -70,6 +68,9 @@ export function Footer() {
               <span className="lg:hidden">Join newsletter</span>
             </Button>
           </form> */}
+            <nav className="mt-11 flex gap-8">
+               <NavLinks /> 
+            </nav>
           <p className="mt-6 text-sm text-gray-500 md:mt-0">
             &copy; Copyright {new Date().getFullYear()}. All rights reserved.
           </p>
